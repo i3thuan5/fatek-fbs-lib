9aea8a5b0cd37395e2c068e2360bfbbe65dc68a0
Marcin Rim <marcin.rim@gmail.com>
2015-10-21 21:44:35 +0200

    Fatek cli tool

--- setup.py
+++ setup.py
@@ -16,7 +16,8 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'pymodbus'
+        'pymodbus',
+        'docopt'
     ],
     scripts=['bin/fatek-cli'],
     classifiers=[
